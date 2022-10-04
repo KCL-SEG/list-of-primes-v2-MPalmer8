4099ecb48d21e692cf2a1ff7d0f51fb5c5ecbd7e
Matthew Palmer <matthewpalmer135@gmail.com>
2022-10-04 15:09:31 +0100

    Add ValueError exception

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
+
+def numIsPrime(n):
+    for num in range(2,int(math.sqrt(n)+ 1)):
+        if n%num == 0:
+            return False
+    return True
+
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError("Only positive numbers are allowed")
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if numIsPrime(num):
+            list.append(num)
+        num += 1
     return list
+
+print(primes(5))
